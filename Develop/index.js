793941a4cbe9af2b912b9ed4b61ff32b224108c5
Easley <jessicaeasley@Jessicas-MacBook-Air.local>
2023-03-02 11:15:41 -0600

    updated index.js file with write function and requiring packages. Started Array of user questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,26 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+      },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Confirmed! Your README file has been generated.")
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
