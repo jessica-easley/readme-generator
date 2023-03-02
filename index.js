b6ef3606c4275d345d4cf06f2fe1d4319c6f991b
Easley <jessicaeasley@Jessicas-MacBook-Air.local>
2023-03-02 13:06:50 -0600

    updated user prompts, updated init app function, updated generateMarkdown file

--- index.js
+++ index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter your project description',
+      },
+      {
+        type: 'input',
+        name: 'install',
+        message: 'Please enter your project installation instructions',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Please choose what type of license you would like',
+      },
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  // console.log(fileName)
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        }
+        // console.log("Confirmed! Your README file has been generated.")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+  .then((answers) => {
+ const readMeText = markdown(answers)
+//  console.log(readMeText)
+ writeToFile('README.md', readMeText)
+})
+ }
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,33 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+console.log(license) 
+if (license != 'none') {
+  return `![License](https://img.shields.io/badge/License-${license}-lightblue.svg)`
+}
+return ''
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// link in the table of contents. not an external link!
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ## ${data.description}
+ ${renderLicenseBadge(data.license)}
+ `;
 }
 
 module.exports = generateMarkdown;
